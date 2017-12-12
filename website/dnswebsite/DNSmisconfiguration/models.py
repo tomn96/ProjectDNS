20d79b417c9a4554bc28faa08517db4cd4f65b24
Tom Nissim <tom.nissim1@mail.huji.ac.il>
2017-12-12 13:44:28 +0200

    added models (for db) of url and ip

--- website/dnswebsite/DNSmisconfiguration/models.py
+++ website/dnswebsite/DNSmisconfiguration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class AddressURL(models.Model):
+    url_address = models.CharField(max_length=256)
+
+
+class AddressIP(models.Model):
+    url = models.ForeignKey(AddressURL, on_delete=models.CASCADE)
+    ip_address = models.CharField(max_length=256)
+    misconfigured = models.BooleanField()
--- website/dnswebsite/dnswebsite/settings.py
+++ website/dnswebsite/dnswebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DNSmisconfiguration.apps.DnsmisconfigurationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
