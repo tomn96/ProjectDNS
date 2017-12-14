ab65af55ca71dbf8a0379d93133ac51210e43097
Tom Nissim <tom.nissim1@mail.huji.ac.il>
2017-12-14 12:07:01 +0200

    new index.html file and urls for address and csv

--- website/dnswebsite/DNSmisconfiguration/urls.py
+++ website/dnswebsite/DNSmisconfiguration/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'DNSmisconfiguration'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('address', views.address, name='address'),
+    path('csv', views.csv, name='csv')
 ]
--- website/dnswebsite/DNSmisconfiguration/views.py
+++ website/dnswebsite/DNSmisconfiguration/views.py
@@ -3,4 +3,27 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("This is the DNSmisconfiguration app")
+
+    members = list()
+    members.append(Member('Tom Nissim'))
+    members.append(Member('Tomer Lior'))
+    members.append(Member('Tom Elyassi'))
+    members.append(Member('Haya Shulman', True))
+
+    context = {'group_members': members}
+
+    return render(request, 'DNSmisconfiguration/index.html', context)
+
+
+class Member:
+    def __init__(self, name, mentor=False):
+        self.name = name
+        self.mentor = mentor
+
+
+def address(request):
+    return HttpResponse("Enter new URL please")
+
+
+def csv(request):
+    return HttpResponse("Enter new CSV file please")
