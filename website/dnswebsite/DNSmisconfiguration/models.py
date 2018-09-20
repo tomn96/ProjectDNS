9254d4f3ff4ec817930ac2aaeda1848c0ed38844
Tom Nissim <tom.nissim1@mail.huji.ac.il>
2018-09-20 14:56:22 +0300

    new models

--- website/dnswebsite/DNSmisconfiguration/models.py
+++ website/dnswebsite/DNSmisconfiguration/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 
-class AddressURL(models.Model):
-    url_address = models.CharField(max_length=256)
+class Server(models.Model):
+    host_name = models.CharField(primary_key=True, max_length=256, default="")
+    description = models.CharField(max_length=512, default="")
+    original_domain = models.CharField(max_length=256, default="")
 
 
-class AddressIP(models.Model):
-    url = models.ForeignKey(AddressURL, on_delete=models.CASCADE)
-    ip_address = models.CharField(max_length=256)
-    misconfigured = models.BooleanField()
+class AddressIPv4(models.Model):
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    ip_address = models.GenericIPAddressField(protocol='IPv4')
+
+
+class AddressIPv6(models.Model):
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    ip_address = models.GenericIPAddressField(protocol='IPv6')
+
+
+class KnownNameServer(models.Model):
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    domain = models.CharField(max_length=512, default="")
+    known_server = models.CharField(max_length=256, default="")
+
+    class Meta:
+        unique_together = (('server', 'domain'),)
